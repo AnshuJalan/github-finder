1236d6af80c7389157e42006f69b14d1f4ae000f
AnshuJalan <anshujalan206@gmail.com>
2020-07-19 11:19:09 +0530

    Pre Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Search from './components/users/Search';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loading: false,
+    users: [],
+    alert: null,
+  };
+
+  //Github User Search
+  searchUsers = async (text) => {
+    this.setState({ loading: true, alert: null });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+    );
+    this.setState({ loading: false, users: res.data.items });
+  };
+
+  //Show Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
+  clearUsers = (e) => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
+    const { users, loading, alert } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Alert alert={alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
